7826e26e5cb23fafe90894e9f66922a3b30e31cc
GabrielHirt <98654562+GabrielHirt@users.noreply.github.com>
2022-08-12 09:15:46 -0300

    Add files via upload

---
+++ scriptt.js
@@ -0,0 +1,6 @@
+var obj = { propriedade_1:   valor_1,   // propriedade_# pode ser um identificador...
+            propriedade_2:   valor_2,   // ou um numero...
+            // ...,
+            propriedade_n: valor_n }; // ou uma string
+
+            console.log(obj.propriedade_1);
